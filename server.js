4a750dac36f2757b89ddd23d49591a7cc8752247
Hoi So <miss.hoiso@gmail.com>
2019-11-13 20:14:27 -0500

    db

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const logger = require('morgan');
+// const logger = require('morgan');
 
-const users = require('./routes/api/users');
+// const users = require('./routes/api/users');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //Make public a static folder 
-app.use(express.static("public"));
-app.use("/api", routes);
+// app.use(express.static("public"));
+// app.use("/api", routes);
 
 
 const db = require("./config/keys").mongoURI;
 mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
-mongoose.connect("mongodb://localhost/gamedb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/gamedb", { useNewUrlParser: true });
 
 
 app.use(passport.initialize());
